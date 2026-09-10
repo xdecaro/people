60df454c5f24c06fdb22223d269da26040e3c5f0
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-10 22:14:23 +0200

    Wire People form to world locations and profile completeness

--- component/admin/src/View/Person/HtmlView.php
+++ component/admin/src/View/Person/HtmlView.php
@@ -7,34 +7,49 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Session\Session;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+use Throwable;
 use xdecaro\Component\People\Administrator\Extension\PeopleComponent;
-use xdecaro\Component\People\Administrator\Service\CountryMetadata;
 
 final class HtmlView extends BaseHtmlView
 {
     public $form;
     public $item;
+    public ?int $profileCompleteness = null;
 
     public function display($tpl = null): void
     {
         $this->form = $this->get('Form');
         $this->item = $this->get('Item');
 
-        $user = Factory::getApplication()->getIdentity();
+        $app = Factory::getApplication();
+        $user = $app->getIdentity();
         $isNew = empty($this->item->id);
         if (!$user->authorise($isNew ? 'core.create' : 'core.edit', 'com_xdecaropeople')) {
             throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
         }
 
-        $component = Factory::getApplication()->bootComponent('com_xdecaropeople');
+        $component = $app->bootComponent('com_xdecaropeople');
         if ($component instanceof PeopleComponent) {
             $component->getCoreIntegrationService()->enableUi($this->document->getWebAssetManager());
+
+            if (!$isNew && ($user->authorise('people.view_sensitive', 'com_xdecaropeople') || $user->authorise('core.admin', 'com_xdecaropeople'))) {
+                try {
+                    $person = $component->getPersonProviderService()->getPerson((int) $this->item->id, true);
+                    $this->profileCompleteness = isset($person['profile_completeness']) ? (int) $person['profile_completeness'] : null;
+                } catch (Throwable) {
+                    $this->profileCompleteness = null;
+                }
+            }
         }
 
         $this->document->addScriptOptions('com_xdecaropeople.person', [
-            'tinBase' => Text::_('COM_XDECAROPEOPLE_FIELD_TIN'),
-            'tinLabels' => CountryMetadata::tinLabels(),
+            'locationUrl' => 'index.php?option=com_xdecaropeople&task=location.search&format=json',
+            'token' => Session::getFormToken(),
+            'locationMinChars' => 2,
+            'locationEmpty' => Text::_('COM_XDECAROPEOPLE_LOCATION_EMPTY'),
+            'locationError' => Text::_('COM_XDECAROPEOPLE_ERROR_WORLD_LOCATION_UNAVAILABLE'),
         ]);
 
         $assets = $this->document->getWebAssetManager();
