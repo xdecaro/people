7805900267b9ec6de8ab57b04a217a2567379526
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-10 03:33:56 +0200

    People 1.1.0: load person form metadata and script

--- component/admin/src/View/Person/HtmlView.php
+++ component/admin/src/View/Person/HtmlView.php
@@ -1,5 +1,52 @@
 <?php
+
 namespace xdecaro\Component\People\Administrator\View\Person;
+
 defined('_JEXEC') or die;
-use Joomla\CMS\Factory;use Joomla\CMS\Language\Text;use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;use Joomla\CMS\Toolbar\ToolbarHelper;use xdecaro\Component\People\Administrator\Extension\PeopleComponent;
-final class HtmlView extends BaseHtmlView{public $form;public $item;public function display($tpl=null):void{$this->form=$this->get('Form');$this->item=$this->get('Item');$u=Factory::getApplication()->getIdentity();$new=empty($this->item->id);if(!$u->authorise($new?'core.create':'core.edit','com_xdecaropeople'))throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'),403);$c=Factory::getApplication()->bootComponent('com_xdecaropeople');if($c instanceof PeopleComponent)$c->getCoreIntegrationService()->enableUi($this->document->getWebAssetManager());$this->document->getWebAssetManager()->useStyle('com_xdecaropeople.admin');ToolbarHelper::title($new?Text::_('COM_XDECAROPEOPLE_PERSON_NEW'):Text::_('COM_XDECAROPEOPLE_PERSON_EDIT'),'user');ToolbarHelper::apply('person.apply');ToolbarHelper::save('person.save');ToolbarHelper::save2new('person.save2new');ToolbarHelper::cancel('person.cancel');parent::display($tpl);}}
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use xdecaro\Component\People\Administrator\Extension\PeopleComponent;
+use xdecaro\Component\People\Administrator\Service\CountryMetadata;
+
+final class HtmlView extends BaseHtmlView
+{
+    public $form;
+    public $item;
+
+    public function display($tpl = null): void
+    {
+        $this->form = $this->get('Form');
+        $this->item = $this->get('Item');
+
+        $user = Factory::getApplication()->getIdentity();
+        $isNew = empty($this->item->id);
+        if (!$user->authorise($isNew ? 'core.create' : 'core.edit', 'com_xdecaropeople')) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        $component = Factory::getApplication()->bootComponent('com_xdecaropeople');
+        if ($component instanceof PeopleComponent) {
+            $component->getCoreIntegrationService()->enableUi($this->document->getWebAssetManager());
+        }
+
+        $this->document->addScriptOptions('com_xdecaropeople.person', [
+            'tinBase' => Text::_('COM_XDECAROPEOPLE_FIELD_TIN'),
+            'tinLabels' => CountryMetadata::tinLabels(),
+        ]);
+
+        $assets = $this->document->getWebAssetManager();
+        $assets->useStyle('com_xdecaropeople.admin');
+        $assets->useScript('com_xdecaropeople.person-form');
+
+        ToolbarHelper::title($isNew ? Text::_('COM_XDECAROPEOPLE_PERSON_NEW') : Text::_('COM_XDECAROPEOPLE_PERSON_EDIT'), 'user');
+        ToolbarHelper::apply('person.apply');
+        ToolbarHelper::save('person.save');
+        ToolbarHelper::save2new('person.save2new');
+        ToolbarHelper::cancel('person.cancel');
+
+        parent::display($tpl);
+    }
+}
