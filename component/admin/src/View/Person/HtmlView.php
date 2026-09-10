6be81b03a085558f297219d663c07b3a6bd25081
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-11 01:20:00 +0200

    Use corrected registered People assets in Joomla 6

--- component/admin/src/View/Person/HtmlView.php
+++ component/admin/src/View/Person/HtmlView.php
@@ -52,18 +52,11 @@ final class HtmlView extends BaseHtmlView
             'locationError' => Text::_('COM_XDECAROPEOPLE_ERROR_WORLD_LOCATION_UNAVAILABLE'),
         ]);
 
+        // Joomla automatically loads this component's joomla.asset.json.
+        // Use the registered assets by name; their URI must omit /css and /js.
         $assets = $this->document->getWebAssetManager();
         $assets->useStyle('com_xdecaropeople.admin');
-
-        // Use a patch-specific asset name so an already registered 1.2.0/1.2.1
-        // definition cannot shadow the hotfix script after an in-place update.
-        $assets->registerAndUseScript(
-            'com_xdecaropeople.person-form-1.2.2',
-            'com_xdecaropeople/js/person-form.js',
-            ['version' => '1.2.2'],
-            ['defer' => true],
-            ['core']
-        );
+        $assets->useScript('com_xdecaropeople.person-form');
 
         ToolbarHelper::title($isNew ? Text::_('COM_XDECAROPEOPLE_PERSON_NEW') : Text::_('COM_XDECAROPEOPLE_PERSON_EDIT'), 'user');
         ToolbarHelper::apply('person.apply');
