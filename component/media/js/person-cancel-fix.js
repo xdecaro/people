7af39f3955b587773622c167efbbf53e628b7124
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-13 23:26:50 +0200

    Align cancel fix asset with People 1.2.9

--- component/media/js/person-cancel-fix.js
+++ component/media/js/person-cancel-fix.js
@@ -3,7 +3,7 @@
 
   const JoomlaApi = window.Joomla || {};
 
-  document.documentElement.dataset.xdecaroPeopleForm = '1.2.7';
+  document.documentElement.dataset.xdecaroPeopleForm = '1.2.9';
 
   if (typeof JoomlaApi.submitbutton !== 'function' || JoomlaApi.__xdecaroPeopleCancelWrapped) {
     return;
