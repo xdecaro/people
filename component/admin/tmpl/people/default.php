004609ea3c41cd10397c0bdfe462487313b5f961
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-23 17:47:32 +0200

    People 1.7.10: remove unsafe All page-size option

--- component/admin/tmpl/people/default.php
+++ component/admin/tmpl/people/default.php
@@ -10,8 +10,7 @@ HTMLHelper::_('behavior.multiselect');
 
 $filterState = (string) $this->state->get('filter.state');
 $currentLimit = (int) $this->state->get('list.limit', 20);
-$limitChoices = [20, 50, 100, 200, 500, 0];
-$totalItems = (int) ($this->pagination->total ?? 0);
+$limitChoices = [20, 50, 100, 200, 500];
 ?>
 <form action="<?php echo Route::_('index.php?option=com_xdecaropeople&view=people'); ?>" method="post" name="adminForm" id="adminForm">
     <div class="xdecaro-scope">
@@ -96,11 +95,7 @@ $totalItems = (int) ($this->pagination->total ?? 0);
                 >
                     <?php foreach ($limitChoices as $limitChoice) : ?>
                         <option value="<?php echo (int) $limitChoice; ?>" <?php echo $currentLimit === $limitChoice ? 'selected' : ''; ?>>
-                            <?php if ($limitChoice === 0) : ?>
-                                <?php echo Text::sprintf('COM_XDECAROPEOPLE_PAGINATION_ALL', $totalItems); ?>
-                            <?php else : ?>
-                                <?php echo (int) $limitChoice; ?>
-                            <?php endif; ?>
+                            <?php echo (int) $limitChoice; ?>
                         </option>
                     <?php endforeach; ?>
                 </select>
