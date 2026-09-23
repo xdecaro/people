48d455abee7d5d447392947ce83b76f93ff1c132
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-23 10:16:56 +0200

    People 1.7.7: add export toolbar action

--- component/admin/src/View/People/HtmlView.php
+++ component/admin/src/View/People/HtmlView.php
@@ -36,7 +36,7 @@ final class HtmlView extends BaseHtmlView
             $component->getCoreIntegrationService()->enableUi($this->document->getWebAssetManager());
         }
 
-        $this->document->getWebAssetManager()->useStyle('com_xdecaropeople.admin');
+        $this->document->getWebAssetManager()\n            ->useStyle('com_xdecaropeople.admin')\n            ->useScript('com_xdecaropeople.export');
 
         ToolbarHelper::title(Text::_('COM_XDECAROPEOPLE_PEOPLE'), 'users');
 
@@ -50,7 +50,7 @@ final class HtmlView extends BaseHtmlView
             }
         }
 
-        if ($user->authorise('core.edit', 'com_xdecaropeople')) {
+        ToolbarHelper::custom('export.open', 'download', '', Text::_('COM_XDECAROPEOPLE_EXPORT'), false);\n\n        if ($user->authorise('core.edit', 'com_xdecaropeople')) {
             ToolbarHelper::editList('person.edit');
         }
 
