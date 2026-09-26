12e4e0002dc81c3926e1763be299d675f6b3d15f
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-26 20:27:26 +0200

    feat: load complete People information center

--- component/admin/src/View/Information/HtmlView.php
+++ component/admin/src/View/Information/HtmlView.php
@@ -1,5 +1,48 @@
 <?php
+
 namespace xdecaro\Component\People\Administrator\View\Information;
+
 defined('_JEXEC') or die;
-use Joomla\CMS\Factory;use Joomla\CMS\Language\Text;use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;use Joomla\CMS\Toolbar\ToolbarHelper;
-final class HtmlView extends BaseHtmlView{public array $diagnostics=[];public function display($tpl=null):void{if(!Factory::getApplication()->getIdentity()->authorise('core.manage','com_xdecaropeople'))throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'),403);$this->diagnostics=(array)$this->get('Diagnostics');ToolbarHelper::title(Text::_('COM_XDECAROPEOPLE_INFORMATION'),'info-circle');parent::display($tpl);}}
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
+final class HtmlView extends BaseHtmlView
+{
+    public array $diagnostics = [];
+    public array $databaseSummary = [];
+    public array $connectedComponents = [];
+    public array $backups = [];
+    public array $recentTrashed = [];
+    public array $maintenanceActivity = [];
+    public bool $canBackup = false;
+    public bool $canRestore = false;
+    public bool $canEditState = false;
+    public bool $canDelete = false;
+
+    public function display($tpl = null): void
+    {
+        $app = Factory::getApplication();
+        $user = $app->getIdentity();
+        if (!$user->authorise('core.manage', 'com_xdecaropeople')) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        $app->getDocument()->getWebAssetManager()->useStyle('com_xdecaropeople.information');
+        $this->diagnostics = (array) $this->get('Diagnostics');
+        $this->databaseSummary = (array) $this->get('DatabaseSummary');
+        $this->connectedComponents = (array) $this->get('ConnectedComponents');
+        $this->backups = (array) $this->get('Backups');
+        $this->recentTrashed = (array) $this->get('RecentTrashed');
+        $this->maintenanceActivity = (array) $this->get('MaintenanceActivity');
+        $this->canBackup = $user->authorise('people.backup', 'com_xdecaropeople');
+        $this->canRestore = $user->authorise('people.restore', 'com_xdecaropeople');
+        $this->canEditState = $user->authorise('core.edit.state', 'com_xdecaropeople');
+        $this->canDelete = $user->authorise('core.delete', 'com_xdecaropeople');
+
+        ToolbarHelper::title(Text::_('COM_XDECAROPEOPLE_INFORMATION'), 'info-circle');
+        parent::display($tpl);
+    }
+}
